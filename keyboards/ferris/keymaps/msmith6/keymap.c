4d0525b36663f82f4104e0b62367b382832f753a
Msmith6 <msmith43@umd.edu>
2021-02-25 00:30:55 -0500

    moved ferris config

--- keyboards/ferris/keymaps/msmith6/keymap.c
+++ keyboards/ferris/keymaps/msmith6/keymap.c
@@ -5,14 +5,14 @@
 #define _LOWER 1
 #define _RAISE 2
 #define _MISC 3
-#define _GAME 4
+#define _MOUSE 4
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   MISC,
-  GAME
+  MOUSE
 };
 
 // Defines for task manager and such
@@ -27,44 +27,56 @@ enum custom_keycodes {
 #define MV_UP LALT(KC_UP) // move up (VS Code)
 #define MV_DOWN LALT(KC_DOWN) // move down (VS Code)
 #define DEL_LINE LCTL(LSFT(KC_K)) // delete line (VS Code)
+#define LWR_J LT(_LOWER, KC_J)
+#define LWR_F LT(_LOWER, KC_F)
+#define LWR_MIN LT(_LOWER, KC_MINS)
+#define LWR_6 LT(_LOWER, KC_6)
+#define RSE_5 LT(_RAISE, KC_5)
+#define RSE_K LT(_RAISE, KC_K)
+#define RSE_D LT(_RAISE, KC_D)
+#define RSE_DWN LT(_RAISE, KC_DOWN)
+#define SFT_SPC RSFT_T(KC_SPC)
+#define SFT_TAB LSFT_T(KC_TAB)
+#define MISC_G LT(_MISC, KC_G)
+#define MISC_H LT(_MISC, KC_H)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_QWERTY] = LAYOUT( \
-  //,-----------------------------------------------.                          ,--------------------------------------------------.
-             KC_Q,         KC_W,  KC_E,  KC_R,  KC_T,                             KC_Y,     KC_U,    KC_I,   KC_O,            KC_P,\
-  //|------------+-------------+------+------+------|                          |------+---------+--------+-------+----------------|
-     LCTL_T(KC_A), LALT_T(KC_S),  KC_D,  KC_F,  KC_G,                             KC_H,     KC_J,    KC_K,   KC_L, RCTL_T(KC_SCLN),\
-  //|------------+-------------+------+------+------|                          |------+---------+--------+-------+----------------|
-             KC_Z,         KC_X,  KC_C,  KC_V,  KC_B,                             KC_N,     KC_M, KC_COMM, KC_DOT,         KC_SLSH,\
-  //|------------+-------------+------+------+------+--------|  |--------------+------+---------+--------+-------+----------------|
-                                               LOWER,    MISC,   LSFT_T(KC_SPC), RAISE \
+  //,-----------------------------------------------.                   ,--------------------------------------------------.
+             KC_Q,         KC_W,  KC_E,  KC_R,  KC_T,                      KC_Y,     KC_U,    KC_I,   KC_O,            KC_P,\
+  //|------------+-------------+------+------+------|                   |------+---------+--------+-------+----------------|
+     LCTL_T(KC_A), LALT_T(KC_S), RSE_D, LWR_F,MISC_G,                    MISC_H,    LWR_J,   RSE_K,   KC_L, RCTL_T(KC_SCLN),\
+  //|------------+-------------+------+------+------|                   |------+---------+--------+-------+----------------|
+     LSFT_T(KC_Z),         KC_X,  KC_C,  KC_V,  KC_B,                      KC_N,     KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),\
+  //|------------+-------------+------+------+------+--------|  |-------+------+---------+--------+-------+----------------|
+                                                MISC,   LOWER,   KC_SPC, RAISE \
                                         //`------------------'  `---------------------'
 
   ),
 
   [_LOWER] = LAYOUT( \
-  //,-------------------------------------------.                    ,---------------------------------------------.
-      KC_ESC,    KC_7,    KC_8,    KC_9, XXXXXXX,                       KC_GRV, KC_QUOT, KC_LPRN, KC_RPRN,KC_BSLASH,\
-  //|-------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
-        KC_0,    KC_4,    KC_5,    KC_6, XXXXXXX,                      XXXXXXX, KC_MINS,  KC_EQL, KC_LBRC,  KC_RBRC,\
-  //|-------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
-     XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX,  KC_DOT,  KC_SLSH,\
-  //|-------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+---------|
-                                           LOWER, XXXXXXX,     KC_SPC,   RAISE \
-                                    //`------------------'  `-----------------'
+  //,-------------------------------------------------.                    ,---------------------------------------------.
+            KC_ESC,    KC_7,    KC_8,    KC_9, XXXXXXX,                       KC_GRV, KC_QUOT, KC_LPRN, KC_RPRN,KC_BSLASH,\
+  //|-------------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+      LCTL_T(KC_0),    KC_4,   RSE_5,   LWR_6, XXXXXXX,                      XXXXXXX, LWR_MIN,  KC_EQL, KC_LBRC,  KC_RBRC,\
+  //|-------------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+           _______,    KC_1,    KC_2,    KC_3, XXXXXXX,                      XXXXXXX, KC_BSPC, KC_COMM,  KC_DOT,  _______,\
+  //|-------------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+---------|
+                                               XXXXXXX,   LOWER,    KC_SPC,   RAISE \
+                                          //`------------------'  `-----------------'
   ),
 
   [_RAISE] = LAYOUT( \
   //,-------------------------------------------.                   ,--------------------------------------------.
       KC_ESC, XXXXXXX, KC_LGUI, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,\
   //|-------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
-     KC_LCTL, KC_LALT, XXXXXXX,  KC_TAB, XXXXXXX,                     XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,\
+     KC_LCTL, KC_LALT, _______, SFT_TAB, XXXXXXX,                     XXXXXXX, KC_LEFT, RSE_DWN,   KC_UP, KC_RGHT,\
   //|-------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_BSPC,  KC_DEL, XXXXXXX,  KC_ENT,\
   //|-------+--------+--------+--------+--------+--------|  |-------+--------+--------+--------+--------+--------|
-                                           LOWER, KC_LSFT,   XXXXXXX,   RAISE \
+                                         XXXXXXX, KC_LSFT,   KC_SPC,   RAISE \
                                     //`------------------'  `----------------'
   ),
 
@@ -72,35 +84,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                    ,--------------------------------------------.
      KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                      XXXXXXX,COL_BLCK,CPY_DOWN,  CPY_UP,EXP_BLCK,\
   //|-----+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-     KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                      XXXXXXX, MV_LEFT, MV_DOWN,   MV_UP, MV_RGHT,\
+     KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,                      _______, MV_LEFT, MV_DOWN,   MV_UP, MV_RGHT,\
   //|-----+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-     KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                         GAME,DEL_LINE, XXXXXXX, XXXXXXX, XXXXXXX,\
+     KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                      XXXXXXX,DEL_LINE, XXXXXXX, XXXXXXX,  MOUSE,\
   //|-----+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
-                                       XXXXXXX,    MISC,    KC_RALT, KC_RCTL \
+                                          MISC, XXXXXXX,    KC_RALT, KC_RCTL \
                                   //`------------------'  `-----------------'
   ),
 
-  [_GAME] = LAYOUT( \
+  [_MOUSE] = LAYOUT( \
   //,--------------------------------------------.                    ,------------------------------------------------------.
-        KC_6,    KC_1,    KC_W,    KC_2,     KC_3,                      KC_PSCR, XXXXXXX, XXXXXXX, RALT(KC_F9), RALT(KC_F10),\
+     KC_WH_L, KC_WH_D, KC_MS_U, KC_WH_U,  KC_WH_R,                      XXXXXXX, KC_WH_L, KC_WH_D,     KC_WH_U,      KC_WH_R,\
   //|-------+--------+--------+--------+---------|                    |--------+--------+--------+------------+-------------|
-     KC_LSFT,    KC_A,    KC_S,    KC_D,     KC_4,                      XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,      XXXXXXX,\
+     KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R,  XXXXXXX,                      XXXXXXX, KC_MS_L, KC_MS_D,     KC_MS_U,      KC_MS_R,\
   //|-------+--------+--------+--------+---------|                    |--------+--------+--------+------------+-------------|
-        KC_7,    KC_Z,    KC_X,    KC_C,     KC_5,                         GAME, XXXXXXX, XXXXXXX,     XXXXXXX,       KC_ESC,\
+     KC_LCTL, KC_BTN5, XXXXXXX, KC_BTN4,  XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,        MOUSE,\
   //|-------+--------+--------+--------+---------+--------|  |--------+--------+--------+--------+------------+-------------|
-                                           KC_TAB,  KC_SPC,    XXXXXXX, QWERTY \
+                                          KC_BTN3, KC_BTN1,    XXXXXXX, QWERTY \
                                      //`------------------'  `----------------'
 
   )
 };
 
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RSFT_T(KC_SPC):
+    case RSE_D:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LSFT_T(KC_Z):
+    case RSFT_T(KC_SLSH):
+      return false;
+    default:
+      return true;
+  }
+}
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case GAME:
+    case MOUSE:
       if (record->event.pressed) {
         layer_clear();
-        layer_on(_GAME);
+        layer_on(_MOUSE);
       }
       return false;
     case QWERTY:
--- keyboards/handwired/ferris/keymaps/msmith6/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define IGNORE_MOD_TAP_INTERRUPT
-#define NO_AUTO_SHIFT_ALPHA
