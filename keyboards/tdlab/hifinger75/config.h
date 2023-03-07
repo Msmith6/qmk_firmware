85362c77a1d83b19058cf375497d490606da3e15
Msmith6 <msmith43@umd.edu>
2023-03-07 14:07:23 -0500

    tdlabs hifinger

---
+++ keyboards/tdlab/hifinger75/config.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "config_common.h"
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 17
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { F0, F1, F4, D1, D0 }
+#define MATRIX_COL_PINS { C7, C6, B6, B5, D2, D6, D7, B4, D4, F7, D3, F6, F5, B0, B3, B2, B1 }
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* number of backlight levels */
+#define BACKLIGHT_PIN B7
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 3
+#define BACKLIGHT_BREATHING
+#endif
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING 0
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#define RGB_DI_PIN E6
+#ifdef RGB_DI_PIN
+#define RGBLED_NUM 13
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_LIMIT_VAL 60
+#endif
